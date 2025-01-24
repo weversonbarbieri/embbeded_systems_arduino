4c23da06060b6a7532da025fe1197f513d47dbd4
weversonbarbieri <weverson.barbieri@gmail.com>
2025-01-23 21:48:38 -0300

    commit the photoresistor files created such as README and .cpp modified

---
+++ photoresistor_7seg_display/README.md
@@ -0,0 +1,49 @@
+# Arduino 7-Segment Display with Photoresistor
+
+![C++](https://img.shields.io/badge/language-C++-blue.svg)
+![Arduino](https://img.shields.io/badge/platform-Arduino-blue.svg)
+
+## Description
+
+This project uses an Arduino to control a 7-segment display based on the input from a photoresistor. The display shows numbers from 0 to 6 depending on the light intensity detected by the photoresistor. Additionally, blue and green LEDs are used to indicate different ranges of light intensity.
+
+## Components Used
+
+  * Arduino board
+  * 7-segment display
+  * Photoresistor
+  * Blue LED
+  * Green LED
+  * Resistors 220 ohms
+  * Breadboard and jumper wires
+
+## Circuit Diagram
+
+(Include a diagram here showing the connections between the Arduino, 7-segment display, photoresistor, and LEDs)
+
+## How It Works
+
+  - The photoresistor measures the ambient light intensity.
+  - The Arduino reads the analog value from the photoresistor.
+  - Based on the value read, the Arduino displays a number (0-6) on the 7-segment display.
+  - The blue and green LEDs indicate different ranges of light intensity:
+  - Blue LED on: Low to medium light intensity.
+  - Green LED on: Medium to high light intensity.
+  - Both LEDs on: High light intensity.
+
+## Installation
+
+  - Connect the components as shown in the circuit diagram.
+  - Upload the provided code to the Arduino using the Arduino IDE.
+  - Open the Serial Monitor to view the photoresistor values and the corresponding number displayed.
+
+## See the System Working
+(Include a link to a video or images showing the system in action)
+
+## Licenses
+
+This project is licensed under the MIT License. See the LICENSE file for more details.
+
+## Contact
+
+For any questions or feedback, please contact [Your Name] at [Your Email].
--- photoresistor_7seg_display/src/main.cpp
+++ photoresistor_7seg_display/src/main.cpp
@@ -1,98 +1,163 @@
 #include <Arduino.h>
 
-int Pinread = A0;
-int valphoto;
-int ledwhite = 11;
-int ledgreen = 12;
-int a = 2;
-int b = 3;
-int c = 4;
-int d = 5;
-int e = 6;
-int f = 7;
-int g = 9;
+// Pin definitions
+int Pinread = A0; // Analog pin to read the photoresistor value
+int valphoto; // Variable to store the photoresistor value
+int ledblue = 11; // Pin for the blue LED
+int ledgreen = 12; // Pin for the green LED
+int a = 2; // Pin for segment 'a' of the 7-segment display
+int b = 3; // Pin for segment 'b' of the 7-segment display
+int c = 4; // Pin for segment 'c' of the 7-segment display
+int d = 5; // Pin for segment 'd' of the 7-segment display
+int e = 6; // Pin for segment 'e' of the 7-segment display
+int f = 7; // Pin for segment 'f' of the 7-segment display
+int g = 9; // Pin for segment 'g' of the 7-segment display
 
 void setup () {
+  // Initialize serial communication at 9600 bps
   Serial.begin(9600);
+
+  // Set the photoresistor pin as input
   pinMode(Pinread, INPUT);
-  pinMode(a, OUTPUT);
-  pinMode(b, OUTPUT);
-  pinMode(c, OUTPUT);
-  pinMode(d, OUTPUT);
-  pinMode(e, OUTPUT);
-  pinMode(f, OUTPUT);
-  pinMode(g, OUTPUT);
-  pinMode(ledwhite, OUTPUT);
+
+  // Array to store the Arduino pin numbers output to the 7-segment display segments
+  int arduino_pins[] = {2, 3, 4, 5, 6, 7, 9};
+
+  // Loop through the arduino_pins array storing the Arduino pin numbers
+  for (int i = 2; i < 10; i++) {
+    // Arduino output based on the loop on the array 
+    pinMode(i, OUTPUT);
+  }
+
+  // Set the LED pins as outputs
+  pinMode(ledblue, OUTPUT);
   pinMode(ledgreen, OUTPUT);
+
 }
 
+// Function to display the number 0 on the 7-segment display
 void zero() {
   digitalWrite(a, HIGH);
-      digitalWrite(b, HIGH);
-      digitalWrite(c, HIGH);
-      digitalWrite(d, HIGH);
-      digitalWrite(e, HIGH);
-      digitalWrite(f, HIGH);
-      digitalWrite(g, LOW);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, LOW);
 }
 
+// Function to display the number 1 on the 7-segment display
 void one() {
-      digitalWrite(a, LOW);
-      digitalWrite(b, HIGH);
-      digitalWrite(c, HIGH);
-      digitalWrite(d, LOW);
-      digitalWrite(e, LOW);
-      digitalWrite(f, LOW);
-      digitalWrite(g, LOW);
+  digitalWrite(a, LOW);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, LOW);
+  digitalWrite(e, LOW);
+  digitalWrite(f, LOW);
+  digitalWrite(g, LOW);
+}
+
+// Function to display the number 2 on the 7-segment display
+void two() {
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, LOW);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, LOW);
+  digitalWrite(g, HIGH);
+}
+
+// Function to display the number 3 on the 7-segment display
+void three() {
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, LOW);
+  digitalWrite(f, LOW);
+  digitalWrite(g, HIGH);
+}
+
+// Function to display the number 4 on the 7-segment display
+void four() {
+  digitalWrite(a, LOW);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, LOW);
+  digitalWrite(e, LOW);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+}
+
+// Function to display the number 5 on the 7-segment display
+void five() {
+  digitalWrite(a, HIGH);
+  digitalWrite(b, LOW);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, LOW);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+}
+
+// Function to display the number 6 on the 7-segment display
+void six() {
+  digitalWrite(a, HIGH);
+  digitalWrite(b, LOW);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
 }
 
 void loop() {
+  // Send the value to the serial monitor
   Serial.println(valphoto);
+
+  // Read the value from the photoresistor
   valphoto = analogRead(Pinread);
+
+  // Delay time to display the photoresistor value on the serial monitor
   delay(500);
-
-  if (valphoto < 150) {
-      zero();
-      digitalWrite(ledwhite, HIGH);
-      digitalWrite(ledgreen, LOW);
-    } else if (valphoto > 150 && valphoto < 300) {
-      one();
-      digitalWrite(ledwhite, HIGH);
-      digitalWrite(ledgreen, LOW);
-    } else if (valphoto > 300 && valphoto < 500) {
-      digitalWrite(a, HIGH);
-      digitalWrite(b, HIGH);
-      digitalWrite(c, LOW);
-      digitalWrite(d, HIGH);
-      digitalWrite(e, HIGH);
-      digitalWrite(f, LOW);
-      digitalWrite(g, HIGH);
-      digitalWrite(ledwhite, LOW);
-      digitalWrite(ledgreen, HIGH);
-    } else if (valphoto > 500 && valphoto < 600) {
-      digitalWrite(a, HIGH);
-      digitalWrite(b, HIGH);
-      digitalWrite(c, HIGH);
-      digitalWrite(d, HIGH);
-      digitalWrite(e, LOW);
-      digitalWrite(f, LOW);
-      digitalWrite(g, HIGH);
-      digitalWrite(ledwhite, LOW);
-      digitalWrite(ledgreen, HIGH);
-    } else {
-      digitalWrite(a, LOW);
-      digitalWrite(b, HIGH);
-      digitalWrite(c, HIGH);
-      digitalWrite(d, LOW);
-      digitalWrite(e, LOW);
-      digitalWrite(f, HIGH);
-      digitalWrite(g, HIGH);
-      digitalWrite(ledwhite, HIGH);
-      digitalWrite(ledgreen, HIGH);
-    
-    }
   
-
-
-
+  // Display a number on the 7-segment display based on the photoresistor value
+  if (valphoto < 100) {
+      // Display de number 0 and turn on blue led and leave the green led off
+      zero();
+      digitalWrite(ledblue, HIGH);
+      digitalWrite(ledgreen, LOW);
+  } else if (valphoto < 200) {
+      // Display de number 1 and turn on blue led and leave the green led off
+      one();
+      digitalWrite(ledblue, HIGH);
+      digitalWrite(ledgreen, LOW);
+  } else if (valphoto < 300) {
+      // Display de number 2 and turn on blue led and leave the green led off
+      two();
+      digitalWrite(ledblue, HIGH);
+      digitalWrite(ledgreen, LOW);
+  } else if (valphoto < 400) {
+      // Display de number 3 and turn off blue led and turn on the green led
+      three();
+      digitalWrite(ledblue, LOW);
+      digitalWrite(ledgreen, HIGH);
+  } else if (valphoto < 500) {
+      // Display de number 4 and turn off blue led and turn on the green led
+      four();
+      digitalWrite(ledblue, LOW);
+      digitalWrite(ledgreen, HIGH);
+  } else if (valphoto < 600) {
+      // Display de number 5 and turn off blue led and turn on the green led
+      five();
+      digitalWrite(ledblue, LOW);
+      digitalWrite(ledgreen, HIGH);
+  } else {
+    // Display de number 6 and turn on both leds 
+    six();
+      digitalWrite(ledblue, HIGH);
+      digitalWrite(ledgreen, HIGH);
+  }
+     
 }
